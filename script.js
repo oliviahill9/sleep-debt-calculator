5bac25b17b5b8065b46758668c3ac006dcf88aae
OFLH <oliviahill99992@gmail.com>
2021-08-16 17:20:57 +0100

    add JavaScript file

---
+++ script.js
@@ -0,0 +1,67 @@
+const getSleepHours = (day) => {
+  switch (day) {
+    case "monday":
+      return 8;
+      break;
+    case "tuesday":
+      return 7;
+      break;
+    case "wednesday":
+      return 9;
+      break;
+    case "thursday":
+      return 6;
+      break;
+    case "friday":
+      return 5;
+      break;
+    case "saturday":
+      return 10;
+      break;
+    case "sunday":
+      return 11;
+      break;
+
+    default:
+      return "Error!";
+  }
+};
+
+const getActualSleepHours = () =>
+  getSleepHours("monday") +
+  getSleepHours("tuesday") +
+  getSleepHours("wednesday") +
+  getSleepHours("thursday") +
+  getSleepHours("friday") +
+  getSleepHours("saturday") +
+  getSleepHours("sunday");
+
+console.log(getSleepHours("monday"));
+
+const getIdealSleepHours = () => {
+  const idealHours = 8;
+  return idealHours * 7;
+};
+
+console.log(getActualSleepHours());
+console.log(getIdealSleepHours());
+
+const calculateSleepDebt = () => {
+  const actualSleepHours = getActualSleepHours();
+  const idealSleepHours = getIdealSleepHours();
+  if (actualSleepHours === idealSleepHours) {
+    console.log("You got the perfect amount of sleep!");
+  } else if (actualSleepHours > idealSleepHours) {
+    console.log(
+      "You got " +
+        (actualSleepHours - idealSleepHours) +
+        " more hours sleep than needed!"
+    );
+  } else {
+    console.log(
+      "You need to get " +
+        (idealSleepHours - actualSleepHours) +
+        "more hours sleep!"
+    );
+  }
+};
